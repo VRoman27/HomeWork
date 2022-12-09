6e275284f408977cac1437e279543946eab3c2e0
VRoman27 <romavolodin1@rambler.ru>
2022-12-09 21:53:02 +0300

    Init and add homework 1-3

---
+++ HomeWork_2/Exam_1/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.*/
+
+bool isParsed = false;
+int num = 0;
+
+while (!isParsed)
+{
+    Console.Write("Введите число: ");
+    isParsed = int.TryParse(Console.ReadLine(), out num);
+    if (isParsed & !(num < 1000 & num > 99))
+    {
+        Console.WriteLine("Не трёхзначное число!");
+        isParsed = false;
+    }
+    else if (!isParsed)
+    {
+        Console.WriteLine("Это не число!");
+    }
+}
+
+int midDigit = (num % 100) / 10;
+
+Console.WriteLine(midDigit);
---
+++ HomeWork_3/Example_1/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.*/
+
+bool isParsed = false;
+int num = 0;
+
+while (!isParsed)
+{
+    Console.Write("Введите число: ");
+    isParsed = int.TryParse(Console.ReadLine(), out num);
+    if (num > 99999 | num < 10000)
+    {
+        Console.WriteLine("Нужно пятизначное число!");
+        isParsed = false;
+    }
+}
+
+ItsPalindrome(num);
+
+int InvertDigitPosition(int number, int numDigit)
+{
+    int newNumber = 0;
+    for (int i = numDigit - 1; i >= 0; i--)
+    {
+        newNumber = newNumber + (number % 10) * (int)Math.Pow(10, (double)i);
+        number /= 10;
+    }
+    return newNumber;
+
+}
+
+void ItsPalindrome(int x)
+{
+    if (x == InvertDigitPosition(x, 5))
+    {
+        Console.WriteLine("Это полиндром");
+    }
+    else
+    {
+        Console.WriteLine("Это не полиндром");
+    }
+}
+
