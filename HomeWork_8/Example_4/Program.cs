37b3c2b601359f0d87d6f2acb97963a016008df0
VRoman27 <romavolodin1@rambler.ru>
2022-12-24 23:54:09 +0300

    add HomeWork_8 ex_4

---
+++ HomeWork_8/Example_4/Program.cs
@@ -0,0 +1,57 @@
+﻿using Common;
+/*Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.*/
+
+int xSize = InputData.IntData("Введите ширину массива: ");
+int ySize = InputData.IntData("Введите длинну массива: ");
+int zSize = InputData.IntData("Введите глубину массива: ");
+int[,,] array = Create3DArrayUniqueElements(xSize, ySize, zSize, (10,100));
+Print3DArray(array);
+
+int[,,] Create3DArrayUniqueElements(int xSize, int ySize, int zSize, (int,int) range)
+{
+    int[,,] array = new int[xSize, ySize, zSize];
+    Random random = new Random();
+    int tmp =random.Next(range.Item1,range.Item2);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                while(AvailabilityValueIn3DArray(array, tmp))
+                {
+                    tmp =random.Next(range.Item1,range.Item2);
+                }
+                array[i,j,k] = tmp;
+            }
+        }
+    }
+    return array;
+}
+
+bool AvailabilityValueIn3DArray(int[,,] array, int value)
+{
+    foreach (var item in array)
+    {
+        if (item==value)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void Print3DArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.WriteLine($"({i},{j},{k}) {array[i, j, k]}");
+            }
+        }
+    }
+}
---
+++ HomeWork_8/Example_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+//Напишите программу, которая заполнит спирально массив 4 на 4.
+
+int line = InputData.IntData("Введите количество строк: ");
+int column = InputData.IntData("Введите количество столбцов: ");
+int[,] array = new int[line, column];
+FillSpiral(array, 0, 0, 1);
+GetArray.Print2DArrayInt(array);
+
+void FillSpiral(int[,] array, int xIndex, int yIndex, int value)
+{
+    if (xIndex >= 0 && yIndex >= 0 && xIndex < array.GetLength(0) && yIndex < array.GetLength(1))
+    {
+        if (array[xIndex, yIndex] == 0)
+        {
+            array[xIndex, yIndex] = value;
+            FillSpiral(array, xIndex, yIndex + 1, value + 1);
+            FillSpiral(array, xIndex + 1, yIndex, value + 1);
+            FillSpiral(array, xIndex, yIndex - 1, value + 1);
+            FillSpiral(array, xIndex - 1, yIndex, value + 1);
+        }
+    }
+
+
+
+}
